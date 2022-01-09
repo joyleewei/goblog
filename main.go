110e4969d7c6e1c8ec179ccdd47e9493e98b76b9
liwei <liwei@dealmap.cloud>
2022-01-09 18:40:46 +0800

    自动加载

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog</h1>")
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 	} else {
-		fmt.Fprint(w, "<h1>请求组页面未找到: (</h1>"+"<p>如有疑惑，请联系我们.</p>")
+		fmt.Fprint(w, "<h1>请求组页面未找到: (</h1>"+"<p>如有疑惑，请联系我们。</p>")
 	}
 }
 
